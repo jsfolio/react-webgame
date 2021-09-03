fba4c88ee8a5c16537c1f6821dd1aa6d7c676faf
jsfolio <jhm8540@daum.net>
2021-09-03 19:59:10 +0900

    webpack

---
+++ wordRelay/word_relay.jsx
@@ -0,0 +1,14 @@
+//npm에서 리액트 불러오기
+const React = require('react');
+const {Component} = React;
+
+class WordRelay extends Component{
+    state = {
+
+    }
+    render(){
+
+    }
+}
+
+module.exports = WordRelay;
